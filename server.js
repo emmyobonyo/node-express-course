bde6618c69dba4c829ae0cd24544ef28be8d598e
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2022-03-30 09:53:11 +0300

    Changes

--- server.js
+++ server.js
@@ -6,21 +6,14 @@ const mockUserData = [
   { name: 'Mark' }
 ]
 
-app.get('/users', function(req,res){
+app.get('/users', function(req, res){
   res.json({
     success: true,
-    message: 'Successfully got users. Nice',
+    message: 'Got the data. Dope',
     users: mockUserData,
   })
 })
 
-// get method also takes two arguments
-// a string (path that will respond to the get function) and a function.
-// listen method takes two arguments.
-// a port number and a function.
-
 app.listen(8000, function(){
   console.log('Server is Running');
 })
-// the listen method takes two arguments.
-// port and a function.
